96335258f4fa6d30cd4556587fbe93d9b17a15b6
Nalini Karna <knalini23@tbc.edu.np>
2025-07-12 14:16:25 +0545

    annotations configuration

--- src/main/java/com/practice/store/OrderService.java
+++ src/main/java/com/practice/store/OrderService.java
@@ -1,8 +1,13 @@
 package com.practice.store;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service  //service is an alias for component annotation however service is used for business logic whereas component is mostly used for utility logic
 public class OrderService {
     private PaymentService paymentService;
 
+    //@Autowired //used when more than one constructors exist to avoid null exception beanexception errors
     public OrderService(PaymentService paymentService){
         this.paymentService =  paymentService;
     }
